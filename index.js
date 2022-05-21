29214a42b835c282e95b3aa86265c65394c0e39a
abedshadin <abedshadin@gmail.com>
2022-05-22 00:11:49 +0600

    tools get all tools

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pkwoc.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+      await client.connect();
+      const toolCollection = client.db("energy-power").collection("tools");
+
+
+      app.get('/tools', async(req,res)=>{
+        const query = {};
+        const tools = await toolCollection.findOne(query);
+        res.send(tools);
+      })
+      
+     
+    } finally {
+  
+    }
+  }
+  run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
