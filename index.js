02c92cacdae9a6befef739ab0cede3df1470e596
abedshadin <abedshadin@gmail.com>
2022-05-22 14:25:55 +0600

    get single data tools

--- index.js
+++ index.js
@@ -1,56 +1,55 @@
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const res = require('express/lib/response');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const res = require("express/lib/response");
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pkwoc.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
 
 async function run() {
-    try {
-      await client.connect();
-      const toolCollection = client.db("energy-power").collection("tools");
-      const reviewCollection = client.db("energy-power").collection("reviews");
+  try {
+    await client.connect();
+    const toolCollection = client.db("energy-power").collection("tools");
+    const reviewCollection = client.db("energy-power").collection("reviews");
 
-//get all tools
-      app.get('/tools', async(req,res)=>{
-        const query = {};
-        const cursor = toolCollection.find(query);
-        const tools = await cursor.toArray();
-        res.send(tools);
-      })
+    //get all tools
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
 
-//get all reviews
-      app.get('/reviews', async(req,res)=>{
-        const query = {};
-        const cursor = reviewCollection.find(query);
-        const reviews = await cursor.toArray();
-        res.send(reviews);
-      })
-      
-     
-    } finally {
-  
-    }
+    //get single tool details
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tools = await toolCollection.findOne(query);
+      res.send(tools);
+    });
+    //get all reviews
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+  } finally {
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
